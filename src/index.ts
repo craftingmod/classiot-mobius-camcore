bc6a0742ff2d4167cd98e6ae905571079475d0e4
craftingmod <alyac1234@gmail.com>
2021-05-23 19:39:43 +0900

    edge test

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,44 @@
-
 import Path from "path"
-
 
 import express from "express"
 import fs from "fs-extra"
-import { Container, Thyme } from "ncube-thyme-typescript"
+import {
+  Container,
+  Thyme,
+  ThymeAE,
+  ThymeContainer,
+  ThymeProtocol,
+} from "ncube-thyme-typescript"
 import cv from "opencv4nodejs"
 
 import { detectFaces } from "./humandetection"
 import { SnapshotCamera } from "./snapshotcamera"
-import { cameraConfig, configPath, createTemp, execAsync, httpPort, inputPath, readConfig, tempPath } from "./util"
-
+import {
+  cameraConfig,
+  configPath,
+  createTemp,
+  execAsync,
+  httpPort,
+  inputPath,
+  readConfig,
+  tempPath,
+} from "./util"
 
 // ensure path
 
 let is_rasp = false
-let snapshot:SnapshotCamera
-let container:Container
+let snapshot: SnapshotCamera
+let container: Container
 let prevData = ""
 
 const httpServer = express()
-const thyme = new Thyme("Mobius", {
-  host: "203.253.128.177",
-  port: 7579,
-  protocol: "http",
+const thyme = new Thyme({
+  main: {
+    type: ThymeProtocol.MQTT,
+    host: "203.253.128.177",
+    port: 1873,
+  },
 })
-
 httpServer.listen(httpPort, () => {
   console.log(`Listening at http://localhost:${httpPort}`)
 })
@@ -48,27 +61,47 @@ httpServer.get("/camera", async (req, res) => {
   }
 })
 
-
 /**
  * Setup before main
  */
+let ae: ThymeAE
+let chairs: ThymeContainer
+let cameraInterval: ThymeContainer
+let cameraIntervalms: number
 async function setup() {
   // connect Thyme
   await thyme.connect()
+  const mobius = await thyme.getCSEBase("Mobius", "Mobius2")
   // get sensor value
-  const ae = await thyme.ensureApplicationEntity("classIoT")
-  container = await thyme.ensureContainer(ae, "camera_human")
+  ae = await mobius.ensureApplicationEntity("schclass01")
+  chairs = await ae.ensureContainer("chair", 1024)
+  cameraInterval = await ae.ensureContainer("camera_interval", 1024)
+  const resp = await cameraInterval.queryLastValue()
+  if (resp.length <= 0) {
+    cameraIntervalms = 1000
+  } else {
+    cameraIntervalms = Number.parseInt(resp)
+  }
+  cameraInterval.on("changed", (value) => {
+    try {
+      cameraIntervalms = Math.max(1000, Number.parseInt(value))
+    } catch (err) {
+      cameraIntervalms = 1000
+    }
+  })
   // ensure dir
   await fs.ensureDir(inputPath)
   await fs.ensureDir(tempPath)
   // check is rasp
-  is_rasp = await readConfig("is_rasp", "true") === "true"
+  is_rasp = (await readConfig("is_rasp", "true")) === "true"
   const remoteIP = await readConfig("remoteip", "127.0.0.1")
   // init snapshotcamera
   snapshot = new SnapshotCamera(is_rasp, `${remoteIP}:${httpPort}`)
   // set camera
   if (is_rasp) {
-    await execAsync(`v4l2-ctl --set-fmt-video=width=${cameraConfig.width},height=${cameraConfig.height},pixelformat=3`)
+    await execAsync(
+      `v4l2-ctl --set-fmt-video=width=${cameraConfig.width},height=${cameraConfig.height},pixelformat=3`
+    )
   }
 }
 
@@ -81,17 +114,35 @@ async function main() {
   const faces = await createTemp("jpg", photo, async (path) => {
     return detectFaces(true, path, path)
   })
-  const sendData = JSON.stringify({
-    ppl: faces.length,
-    loc: faces.map((v) => [...v.location, ...v.size]),
-  })
+  const people = [0, 0, 0, 0, 0, 0]
+  const xedges = [0.33, 0.66, 1]
+  const yedges = [0.33, 0.66, 1]
+  for (const face of faces) {
+    let detected = false
+    for (let x = 0; x < xedges.length; x += 1) {
+      const xedge_abs = cameraConfig.width * xedges[x]
+      for (let y = 0; y < yedges.length; y += 1) {
+        const yedge_abs = cameraConfig.height * yedges[y]
+        if (face.center[0] < xedge_abs && face.center[1] < yedge_abs) {
+          detected = true
+          people[yedges.length * y + x] += 1
+          break
+        }
+      }
+      if (detected) {
+        break
+      }
+    }
+  }
+
+  const sendData = JSON.stringify(people)
   if (prevData != sendData) {
     prevData = sendData
     console.log(sendData)
-    await thyme.addContentInstance(container, sendData)
+    await chairs.addContentInstance(sendData)
   }
 
-  setTimeout(main, Math.max(200, 1000-(Date.now() - start)))
+  setTimeout(main, Math.max(200, cameraIntervalms - (Date.now() - start)))
 
   // const img = await cv.imreadAsync(Path.resolve(inputPath, "sample1.png"))
   // const grayImg = await img.bgrToGrayAsync()
@@ -117,4 +168,3 @@ try {
 } catch (err) {
   console.error(err)
 }
-
